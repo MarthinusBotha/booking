b3fc4f341e7337b858026e4ca0c030989fbc7399
Neduko <thinimab@gmail.com>
2023-10-27 03:31:34 +0200

    Updated field name

--- database/migrations/2023_10_26_193920_create_booking_items_table.php
+++ database/migrations/2023_10_26_193920_create_booking_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('booking_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('title', 100);
             $table->text('description');
             $table->decimal('price', 10, 2);
             $table->unsignedTinyInteger('guests');
